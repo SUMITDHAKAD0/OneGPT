6b66616c255497d58e2aeca5328ae5d48fd52c47
Sumit Dhakad <sumit.dhakad@testaing.com>
2024-09-29 01:52:26 +0530

    data loader file added

---
+++ src/data_ingestion/data_loader.py
@@ -0,0 +1,60 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import Docx2txtLoader
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import TextSplitter, CharacterTextSplitter
+from src.logging import logger
+import os
+import sys
+
+class Loader:
+    def __init__(self):
+        self.path = "data/gst1.pdf"
+
+    def pdfloader(self):
+        if not self.path:
+            raise ValueError("No file path provided.")
+        try:
+            logger.info(f"Loading {os.path.basename(self.path)} File")
+            pdf_loader = PyPDFLoader(self.path)
+            pdf_content = pdf_loader.load()
+            return pdf_content
+        except Exception as e:
+            logger.error(f"Error: File not found - {self.path}")
+            print(e)
+            sys.exit(1)
+            
+
+    def docloader(self):
+        if not self.path:
+            raise ValueError("No file path provided.")
+        try:
+            logger.info(f"Loading {os.path.basename(self.path)} File")
+            doc_loader = Docx2txtLoader(self.path)
+            doc_content = doc_loader.load()
+            return doc_content
+        except Exception as e:
+            logger.error(f"Error: File not found - {self.path}")
+            print(e)
+            sys.exit(1)
+        
+
+    def textloader(self):
+        if not self.path:
+            raise ValueError("No file path provided.")
+        try:
+            logger.info(f"Loading {os.path.basename(self.path)} File")
+            text_loader = TextLoader(self.path)
+            text_content = text_loader.load()
+            return text_content
+        except Exception as e:
+            logger.error(f"Error: File not found - {self.path}")
+            print(e)
+            sys.exit(1)
+        
+
+if __name__ == "__main__":
+    data = Loader().pdfloader()
+    print(data)
+
---
+++ test.py
@@ -0,0 +1,10 @@
+# from onegpt.data_ingestion import data_loader
+from src.data_ingestion.data_loader import Loader
+# from src.data_loader import Loader
+# from src.logging.data_loader import Loader
+from src.logging import logger
+
+data = Loader().pdfloader()
+print(data)
+
+logger.info('All Good Buddy!!!')
