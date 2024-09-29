c3a6aacc472fb532590ae3ccc1be41ac6028adb5
Sumit Dhakad <sumit.dhakad@testaing.com>
2024-09-29 18:33:34 +0530

    bug fix

--- src/data_ingestion/data_loader.py
+++ src/data_ingestion/data_loader.py
@@ -69,6 +69,19 @@ class Loader:
             logger.error(f"Error: File not found - {self.path}")
             print(e)
             sys.exit(1)
+    
+    def webloader(self):
+        if not self.path:
+            raise ValueError("No file path provided.")
+        try:
+            logger.info(f"Loading {os.path.basename(self.path)} File")
+            web_loader = WebBaseLoader(self.path)
+            web_content = web_loader.load()
+            return web_content
+        except Exception as e:
+            logger.error(f"Error: File not found - {self.path}")
+            print(e)
+            sys.exit(1)
 
 
 if __name__ == "__main__":
